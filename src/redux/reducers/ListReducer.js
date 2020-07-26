eec36a1abc1f81421b28a2d5b7b4d0138e619447
Tala Agha <macbook@MacBooks-MacBook-Pro.local>
2020-07-26 12:20:09 +0300

    second day

--- src/redux/reducers/ListReducer.js
+++ src/redux/reducers/ListReducer.js
@@ -2,36 +2,49 @@ import { ADD_ITEM, DELETE_ITEM } from "../actions/actionTypes";
 
 const inistialState = {
   inboxList: JSON.parse(localStorage.getItem("inboxList"))
-  ? JSON.parse(localStorage.getItem("inboxList"))
-  : [],
-  listList:[],
+    ? JSON.parse(localStorage.getItem("inboxList"))
+    : [],
+  listList: JSON.parse(localStorage.getItem("listList"))
+    ? JSON.parse(localStorage.getItem("listList"))
+    : [],
 };
 
 const reducer = (state = inistialState, action) => {
   switch (action.type) {
     case ADD_ITEM:
-      const done = action.payload.startsWith("x ")? true : false
-      const cancel = action.payload.startsWith("~ ")? true : false
-      const item = action.payload.startsWith("x ")||action.payload.startsWith("~ ")? action.payload.substring(2):action.payload
-      const newItem = { title: item, done: done , cancel:cancel};
-      let newList =[]
-      if (newItem.title.includes("#")){
-         newList = state.listList.concat(newItem)
+      const done = action.payload.startsWith("x ") ? true : false;
+      const cancel = action.payload.startsWith("~ ") ? true : false;
+      const item =
+        action.payload.startsWith("x ") || action.payload.startsWith("~ ")
+          ? action.payload.substring(2)
+          : action.payload;
+      const newItem = { title: item, done: done, cancel: cancel };
+      let newList = state.listList;
+      if (newItem.title.includes("#")) {
+        newList = newList.concat(newItem);
+        localStorage.setItem("listList", JSON.stringify(newList));
       }
-      const newInbox = state.inboxList.concat(newItem)
+      const newInbox = state.inboxList.concat(newItem);
       localStorage.setItem("inboxList", JSON.stringify(newInbox));
       return {
         ...state,
         inboxList: newInbox,
-        listList: newList
+        listList: newList,
       };
     case DELETE_ITEM:
       const deleteItem = action.payload;
-      const InboxAfterDelete = state.inboxList.filter((item) => item !== deleteItem)
+      const InboxAfterDelete = state.inboxList.filter(
+        (item) => item !== deleteItem
+      );
+      const ListAfterDelete = state.listList.filter(
+        (item) => item !== deleteItem
+      );
       localStorage.setItem("inboxList", JSON.stringify(InboxAfterDelete));
+      localStorage.setItem("listList", JSON.stringify(ListAfterDelete));
       return {
         ...state,
         inboxList: InboxAfterDelete,
+        listList: ListAfterDelete,
       };
     default:
       return {
