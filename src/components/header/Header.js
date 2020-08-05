97bb707452f36b4c24226ea05010d82863009a16
abood shaheen <shaheen.abdelmajeed@outlook.com>
2020-08-05 12:08:14 +0300

    HEaders and search

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,9 @@
 import React from "react"
 
-const Header = (props)=>{
+const Header = ()=>{
     return(
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
       <span className="navbar-brand" >Grace</span>
-      <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-        <li className="nav-item active">
-        </li>
-        <li className="nav-item">
-        </li>
-      </ul>
-        <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Add List</button>
-    </div>
   </nav>
   )
 }
